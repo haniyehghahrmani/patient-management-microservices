a88cbccca26e3c75f51797778ef6c97c6214446c
Asus <haniyehghahrmani@gmail.com>
2025-12-20 14:59:21 +0330

    init: initial patient service setup

---
+++ patient-service/src/main/java/com/hani/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,31 @@
+package com.hani.patientservice.mapper;
+
+import com.hani.patientservice.dto.PatientRequestDTO;
+import com.hani.patientservice.dto.PatientResponseDTO;
+import com.hani.patientservice.model.Patient;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setName(patient.getName());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+        patientDTO.setRegisteredDate(patient.getRegisteredDate().toString());
+        return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
+    }
+}
