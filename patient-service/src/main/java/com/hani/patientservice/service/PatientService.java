e69aaaf8c90cf1ded76c3f17610fe747f0d46d8c
Asus <haniyehghahrmani@gmail.com>
2025-12-21 18:16:01 +0330

    feat: publish patient events to Kafka

--- patient-service/src/main/java/com/hani/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/hani/patientservice/service/PatientService.java
@@ -5,6 +5,7 @@ import com.hani.patientservice.dto.PatientResponseDTO;
 import com.hani.patientservice.exception.EmailAlreadyExistsException;
 import com.hani.patientservice.exception.PatientNotFoundException;
 import com.hani.patientservice.grpc.BillingServiceGrpcClient;
+import com.hani.patientservice.kafka.KafkaProducer;
 import com.hani.patientservice.mapper.PatientMapper;
 import com.hani.patientservice.model.Patient;
 import com.hani.patientservice.repository.PatientRepository;
@@ -19,10 +20,12 @@ public class PatientService {
 
     private final PatientRepository patientRepository;
     private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final KafkaProducer kafkaProducer;
 
-    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient,KafkaProducer kafkaProducer) {
         this.patientRepository = patientRepository;
         this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.kafkaProducer = kafkaProducer;
     }
 
     public List<PatientResponseDTO> getAllPatients() {
@@ -38,6 +41,8 @@ public class PatientService {
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
 
         billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(), newPatient.getName(), newPatient.getEmail());
+
+        kafkaProducer.sendEvent(newPatient);
 
         return PatientMapper.toDTO(newPatient);
     }
